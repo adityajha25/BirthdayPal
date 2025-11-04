b9fde94e003443984285ff6bb873fd5d527e0c26
Aditya Jha <to.aditya.jha@proton.me>
2025-11-03 19:29:19 -0500

    kobe

---
+++ Birthday/View/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  Birthday
+//
+//  Created by Aditya Jha    on 11/3/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView();
+        }
+    }
+}
